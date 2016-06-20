c792ce2b2764d93f7e297085be3654f9e855e936
mrRachar <matthewross@campars.net>
2016-06-20 16:25:08 +0100

    Now supports multiple file uploads at once, just use "[]" at the end of the variable name, and specify enctype="multipart/form-data" as a form attribute

--- AElfi/request.py
+++ AElfi/request.py
@@ -183,16 +183,29 @@ class Request:
                 arg = nextline(arg)
                 if filename is None:
                     arg = nextline(arg)[:-1]
-                    name = name.decode(config.charset)
-                    post[name[1:-1]] = arg.decode(config.charset) if arg.decode(config.charset)[-1] != '\r' else arg.decode(config.charset)[:-1]
+                    arg = arg.decode(config.charset) if arg.decode(config.charset)[-1] != '\r' else arg.decode(config.charset)[:-1]
+                    name = name.decode(config.charset)[1:-1]
+                    if name.endswith('[]'):
+                        if name in post:
+                            post[name].append(arg)
+                        else:
+                            post[name] = [arg]
+                    else:
+                        post[name] = arg
                     continue
-                name, filename = name.decode(config.charset), filename.decode(config.charset)
+                name, filename = name.decode(config.charset)[1:-1], filename.decode(config.charset)
                 contenttype = re.match(rb'Content-Type:\s*([A-Za-z0-9/-_:]+)', arg).group(1).decode(config.charset)
                 uploaded_file = UploadedFile(name=filename[1:-1], contenttype=contenttype)
                 arg = nextline(nextline(arg))[:-1]
                 uploaded_file.file.write(arg)
                 uploaded_file.file.seek(0)
-                post[name[1:-1]] = uploaded_file
+                if name.endswith('[]'):
+                    if name in post:
+                        post[name].append(uploaded_file)
+                    else:
+                        post[name] = [uploaded_file]
+                else:
+                    post[name] = uploaded_file
             return post
         else:
             querystring = querystring.decode(config.charset)
